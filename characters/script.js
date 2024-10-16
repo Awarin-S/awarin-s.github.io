776982a6dd7d2542a18bb048133357328de576b0
Paddfoot <awarin.site@gmail.com>
2024-10-16 06:01:39 +0400

    t

---
+++ characters/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('open');
+}
+
+const jsonUrl = 'https://raw.githubusercontent.com/Paddfoot/Paddfoot.github.io/main/data/data.json';
